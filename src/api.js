910a5dddfc3ab266a0f229086ff11b2253fd7fbc
Subham Raj <rajshubham199@gmail.com>
2025-03-22 17:53:58 +0530

    updated backend live-link

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api/forms",
+    // baseURL: "http://localhost:5000/api/forms",
+    baseURL: "https://formbuilder-backend-7g5u.onrender.com/api/forms",
     headers: { "Content-Type": "application/json" }
 });
 
